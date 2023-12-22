e6c68f67e865ee96c22477e7ae1b7701a1f7bf7c
ZeroMan2002 <51942834+ZeroMan2002@users.noreply.github.com>
2023-12-22 00:05:41 +0000

    Public

--- sugestion.js
+++
@@ -1,9 +0,0 @@
-// Example bot.js
-const { Client } = require('discord.js');
-const client = new Client();
-
-client.once('ready', () => {
-  console.log('Bot is ready!');
-});
-
-client.login(process.env.BOT_TOKEN);
